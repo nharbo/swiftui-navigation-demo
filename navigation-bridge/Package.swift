a2a39fdf84b4f8e6369ecb5d1b51c459e6b584ac
Nicolai Harbo <niha@trifork.com>
2024-01-12 15:09:30 +0100

    now building again :)

---
+++ navigation-bridge/Package.swift
@@ -0,0 +1,34 @@
+// swift-tools-version: 5.9
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+
+import PackageDescription
+
+let package = Package(
+    name: "navigation-bridge",
+    platforms: [.iOS(.v16)],
+    products: [
+        // Products define the executables and libraries a package produces, making them visible to other packages.
+        .library(
+            name: "NavigationBridge",
+            targets: ["NavigationBridge"]),
+    ],
+    dependencies: [
+        .package(path: "../models"),
+        .package(path: "../navigation"),
+        .package(path: "../packageOne"),
+        .package(path: "../packageTwo"),
+    ],
+    targets: [
+        // Targets are the basic building blocks of a package, defining a module or a test suite.
+        // Targets can depend on other targets in this package and products from dependencies.
+        .target(
+            name: "NavigationBridge",
+            dependencies: [
+                .product(name: "Models", package: "models"),
+                .product(name: "Navigation", package: "navigation"),
+                .product(name: "PackageOne", package: "packageOne"),
+                .product(name: "PackageTwo", package: "packageTwo")
+            ]
+        )
+    ]
+)
---
+++ packageOne/Sources/PackageOne/Navigation/ViewProvider.swift
@@ -0,0 +1,8 @@
+//
+//  File.swift
+//  
+//
+//  Created by Nicolai Harbo on 12/01/2024.
+//
+
+import Foundation
