8318760fcd15d78e08d49786f8117cb426e29fe2
Nicolai Harbo <niha@trifork.com>
2024-01-15 14:50:15 +0100

    building

--- navigation-bridge/Package.swift
+++ navigation-bridge/Package.swift
@@ -5,7 +5,6 @@ import PackageDescription
 
 let package = Package(
     name: "navigation-bridge",
-    platforms: [.iOS(.v16)],
     products: [
         // Products define the executables and libraries a package produces, making them visible to other packages.
         .library(
@@ -14,9 +13,8 @@ let package = Package(
     ],
     dependencies: [
         .package(path: "../models"),
-        .package(path: "../navigation"),
         .package(path: "../packageOne"),
-        .package(path: "../packageTwo"),
+        .package(path: "../packageTwo")
     ],
     targets: [
         // Targets are the basic building blocks of a package, defining a module or a test suite.
@@ -25,7 +23,6 @@ let package = Package(
             name: "NavigationBridge",
             dependencies: [
                 .product(name: "Models", package: "models"),
-                .product(name: "Navigation", package: "navigation"),
                 .product(name: "PackageOne", package: "packageOne"),
                 .product(name: "PackageTwo", package: "packageTwo")
             ]
